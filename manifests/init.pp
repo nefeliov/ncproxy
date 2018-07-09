9a361bddf723512c40d816ab2654d1e1cd7652d1
Oscar Vidakovic <vidakovic@gmail.com>
2018-07-09 02:31:18 +0200

    Bugfix: fixed incorrect trailing commas. Bugfix: enable servers (aka vhosts).

--- manifests/init.pp
+++ manifests/init.pp
@@ -22,30 +22,32 @@ class ncproxy {
         enabled_sites_manage => false,
         configs              => hiera_hash('nginx::configs', {}),
         vhosts               => hiera_hash('nginx::vhosts', {}),
-        },
-        
-        # TODO: As I'm not sure if enabled_sites_manage will purge all sites enabled 
-        # and recreate symlinks on each puppet invocation if all information is not 
-        # included in the hiera_hash in the nginx class invokation, so I preferred
-        # set it to false and ensure default site is deleted here
-        file { '/etc/nginx/sites-enabled/default':
-            ensure => absent,
-        }
-        
-        nginx::resource::config { 'proxy_log.conf':
-            source => 'puppet:///modules/ncproxy/proxy_log.conf'}),
-        },
+    }
+    
+    # TODO: As I'm not sure if enabled_sites_manage will purge all sites enabled 
+    # and recreate symlinks on each puppet invocation if all information is not 
+    # included in the hiera_hash in the nginx class invokation, so I preferred
+    # set it to false and ensure default site is deleted here
+    file { '/etc/nginx/sites-enabled/default':
+        ensure => absent,
+    }
+    
+    nginx::resource::config { 'proxy_log.conf':
+        source => 'puppet:///modules/ncproxy/proxy_log.conf'}),
+    }
 
-        nginx::resource::config { 'forward-headers.conf':
-            source => 'puppet:///modules/ncproxy/forward-headers.conf'}),
-        },
+    nginx::resource::config { 'forward-headers.conf':
+        source => 'puppet:///modules/ncproxy/forward-headers.conf'}),
+    }
 
-        nginx::resource::vhosts { '01_reverse_proxy':
-            source => 'puppet:///modules/ncproxy/01_reverse_proxy'}),
-        },
+    nginx::resource::vhosts { '01_reverse_proxy':
+        enabled => true,
+        source => 'puppet:///modules/ncproxy/01_reverse_proxy'}),
+    }
 
-        nginx::resource::vhosts { '02_forward_proxy':
-            source => 'puppet:///modules/ncproxy/02_forward_proxy'}),
-        },
+    nginx::resource::vhosts { '02_forward_proxy':
+        enabled => true,
+        source => 'puppet:///modules/ncproxy/02_forward_proxy'}),
+    }
         
 }
